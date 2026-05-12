d97396fd4472a09927906d5d6648629f54ec9940
Siddhartha Vyas <siddharthavyas67@gmail.com>
2026-05-12 15:59:07 +0530

    implemented home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,90 @@
 import "../../global.css";
-import { Text } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import { icons } from "@/constants/icons";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "@/constants/data";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import ListHeading from "@/components/ListHeading";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import { useState } from "react";
 
 export default function App() {
+  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<
+    string | null
+  >(null);
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold">
-        Home
-      </Text>
-
-      <Link
-        href="/onBoarding"
-        className="mt-4 font-sans-bold rounded bg-primary px-4 py-3"
-      >
-        <Text className="text-white">Go to Onboarding</Text>
-      </Link>
-
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 font-sans-bold rounded bg-primary px-4 py-3"
-      >
-        <Text className="text-white">Go to Sign In</Text>
-      </Link>
-
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 font-sans-bold rounded bg-primary px-4 py-3"
-      >
-        <Text className="text-white">Go to Sign Up</Text>
-      </Link>
-
+      
+      <FlatList
+        ListHeaderComponent={() => (
+          <>
+            <View className="home-header">
+              <View className="home-user">
+                <Image source={images.avatar} className="home-avatar" />
+                <Text className="home-user-name">{HOME_USER.name}</Text>
+              </View>
+              <Image source={icons.add} className="home-add-icon" />
+            </View>
+            <View className="home-balance-card">
+              <Text className="home-balance-label"> Balance</Text>
+              <View className="home-balance-row">
+                <Text className="home-balance-amount">
+                  {formatCurrency(HOME_BALANCE.amount)}
+                </Text>
+                <Text className="home-balance-date">
+                  {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/D")}
+                </Text>
+              </View>
+            </View>
+            <View className="mb-5">
+              <ListHeading title="Upcoming Renewals" />
+              <FlatList
+                data={UPCOMING_SUBSCRIPTIONS}
+                renderItem={({ item }) => (
+                  <UpcomingSubscriptionCard {...item} />
+                )}
+                keyExtractor={(item) => item.id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ListEmptyComponent={
+                  <Text className="home-empty-state">
+                    No upcoming subscriptions
+                  </Text>
+                }
+              />
+            </View>
+            <ListHeading title="All Subscriptions" />
+          </>
+        )}
+        data={HOME_SUBSCRIPTIONS}
+        renderItem={({ item }) => (
+          <SubscriptionCard
+            {...item}
+            expanded={expandedSubscriptionId === item.id}
+            onPress={() =>
+              setExpandedSubscriptionId(
+                expandedSubscriptionId === item.id ? null : item.id,
+              )
+            }
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        extraData={expandedSubscriptionId}
+        ItemSeparatorComponent={() => <View className="h-4" />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <Text className="home-empty-state">No subscriptions found</Text>
+        }
+      />
     </SafeAreaView>
   );
 }
